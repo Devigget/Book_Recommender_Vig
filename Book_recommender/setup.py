bfe22392d5499c07ebbb1fc5236f1eee43bbb213
Devigget <vigneshgaunker@gmail.com>
2024-10-01 14:14:30 +0530

    Update setup.py

--- Book_recommender/setup.py
+++ Book_recommender/setup.py
@@ -1,24 +1,16 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
-
-## edit below variables as per your requirements -
-REPO_NAME = "ML Based Books Recommender System"
-AUTHOR_USER_NAME = "Vignesh Gaunker"
-SRC_REPO = "books_recommender"
-LIST_OF_REQUIREMENTS = []
-
-
 setup(
-    name=SRC_REPO,
-    version="0.0.1",
-    author="Vignesh Gaunker",
-    description="A small local packages for ML based books recommendations",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author_email="boktiar@ineuron.ai",
+    name='book_recommender',
+    version='0.1',
     packages=find_packages(),
-    python_requires=">=3.7",
-    install_requires=LIST_OF_REQUIREMENTS
+    install_requires=[
+        'streamlit',
+        'numpy',
+        'pandas',
+        'scikit-learn',
+        'seaborn',
+        'scipy',
+        'matplotlib',
+    ],
 )
